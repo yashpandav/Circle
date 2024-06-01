ead13575e47045d013180e7565b141f181c41e5a
yashpandav <pandavyash076@gmail.com>
2024-06-01 22:42:08 +0530

    Finally Hope ToDo Complete

--- Controllers/ToDoControllers/addAss.js
+++ Controllers/ToDoControllers/addAss.js
@@ -5,10 +5,35 @@ const Class = require('../../Models/Class');
 const cron = require('cron');
 const SubmitAssignment = require('../../Models/SubmitAssignment');
 
-async function updateToDo (req , res) {
+async function fetchClassAssignments(classId, userId) {
+    const currClass = await Class.findById(classId).populate("addedAssignment").exec();
+    if (!currClass || !currClass.addedAssignment) return null;
+
+    const assigned = [];
+    const missing = [];
+    const completed = [];
+
+    await Promise.all(currClass.addedAssignment.map(
+        async (assignment) => {
+            const currAssignment = await Assignment.findById(assignment.id).populate("submission");
+            const submission = currAssignment.submission.find(sub => sub.student.equals(userId));
+
+            if (!submission && new Date(currAssignment.dueDate) > new Date()) {
+                assigned.push(currAssignment.id);
+            } else if (!submission && new Date(currAssignment.dueDate) < new Date()) {
+                missing.push(currAssignment.id);
+            } else if (submission) {
+                completed.push(currAssignment.id);
+            }
+    }));
+    return { classId, assigned, missing, completed };
+}
+
+async function updateToDo(req, res) {
     try {
         const userId = req.user.id;
         const joinedClasses = req.joinedClassAsStudent;
+        const classId = req.params.claId;
 
         if (!joinedClasses || joinedClasses.length === 0) {
             return res.status(400).json({
@@ -25,52 +50,19 @@ async function updateToDo (req , res) {
             });
         }
 
-        let assigned = [];
-        let missing = [];
-        let completed = [];
+        const classIds = claId ? [claId] : joinedClasses;
 
-        console.log(joinedClasses);
-
-        await Promise.all(joinedClasses
-            .map((
-                async (classId) => {
-                    const currClass = await Class.findById(classId).populate("addedAssignment").exec();
-
-                    if(currClass && currClass.addedAssignment){
-
-                        await Promise.all(currClass.addedAssignment.map(
-                            async (assignment) => {
-                                const currAssignment = await Assignment.findById(assignment.id).populate("submission");
-
-                                const submission = currAssignment.submission.find(sub => sub.student.equals(userId))
-
-                                if(!submission && new Date(currAssignment.dueDate) > new Date()){
-                                    assigned.push(currAssignment.id);   
-                                }
-                                else if(!submission && new Date(currAssignment.dueDate) < new Date()){
-                                    missing.push(currAssignment.id);
-                                }
-                                else if(submission){
-                                    completed.push(currAssignment.id);
-                                }
-                            }
-                        ))
-                    }
-                }
-            ))
-        );
+        const allAssignments = await Promise.all(classIds.map(classId => fetchClassAssignments(classId, userId)));
+        const assignmentsByClass = allAssignments.filter(Boolean); // Remove null values
 
         let toDo = await ToDo.findById(user.todo);
-        if(!toDo){
+        if (!toDo) {
             toDo = new ToDo({
-                assigned,
-                missing,
-                completed,
+                user: userId,
+                byClass: assignmentsByClass,
             });
-        }else {
-            toDo.assigned = assigned;
-            toDo.missing = missing;
-            toDo.completed = completed;
+        } else {
+            toDo.byClass = assignmentsByClass;
         }
 
         await toDo.save();
@@ -91,4 +83,4 @@ async function updateToDo (req , res) {
 
 cron.schedule('0 0 * * *', updateToDo);
 
-module.exports = { updateToDo };
+module.exports = { updateToDo };s
--- Routes/ToDosRoutes.js
+++ Routes/ToDosRoutes.js
@@ -1,8 +1,8 @@
 const router = require('express').Router();
 
-const { auth , isStudnet} = require('../Middleware/auth');
+const { auth } = require('../Middleware/auth');
 
 const { updateToDo } = require('../Controllers/ToDoControllers/addAss');
-router.post('/todos' , auth , updateToDo);
+router.post('/todos:classId' , auth , updateToDo);
 
 module.exports = router;
