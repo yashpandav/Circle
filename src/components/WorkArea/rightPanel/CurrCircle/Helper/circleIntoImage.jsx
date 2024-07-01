a0ac96645f72c8b2acf6a43c357f8216a17cfed0
yashpandav <pandavyash076@gmail.com>
2024-07-01 22:32:15 +0530

    Make more nicer main banner image

--- src/components/WorkArea/rightPanel/CurrCircle/Helper/circleIntoImage.jsx
+++ src/components/WorkArea/rightPanel/CurrCircle/Helper/circleIntoImage.jsx
@@ -1,16 +1,20 @@
 import React from "react";
 import { useSelector } from "react-redux";
-import './cirleIntroImage.css'
+import InfoTwoToneIcon from '@mui/icons-material/InfoTwoTone';
+import './circleIntroImage.css';
 
 export default function CircleIntroImage() {
     const currClass = useSelector((state) => state.classes.currClass);
 
     return (
         <div
-            id="curr-circle-informer"
-            style={{ backgroundImage: `url(${currClass.thumbnail})`}}
+            className="circle-intro-image"
+            style={{ backgroundImage: `url(${currClass.thumbnail})` }}
         >
-            <h3 id="curr-circle-name">{currClass.name}</h3>
+            <div className="circle-content">
+                <h3 className="curr-circle-name">{currClass.name}</h3>
+                <InfoTwoToneIcon className="info-icon" />
+            </div>
         </div>
-    )
+    );
 }
