6addd196bbe8db9c2ca03fafde456c64b2cdf7c1
yashpandav <pandavyash076@gmail.com>
2024-07-06 22:28:21 +0530

    Made Nice Text Area

--- src/components/WorkArea/rightPanel/CurrCircle/Helper/MainCircleAreaHelper/RightSideHelper/announcementContainer.jsx
+++ src/components/WorkArea/rightPanel/CurrCircle/Helper/MainCircleAreaHelper/RightSideHelper/announcementContainer.jsx
@@ -1,53 +1,104 @@
 import React, { useState } from "react";
-import { TextField, Button } from "@mui/material";
+import { TextField, Button, IconButton } from "@mui/material";
 import { useSelector } from "react-redux";
+import { IoIosSend } from "react-icons/io";
+import {
+    FormatBold,
+    FormatItalic,
+    FormatUnderlined,
+    CloudUpload,
+    Send,
+} from "@mui/icons-material";
 import "./announcementContainer.css";
 
-const UserAnnouncementHeader = () => {
+const UserAnnouncementHeader = ({ setWriteAssignment }) => {
     const user = useSelector((state) => state.auth.user);
     const currClass = useSelector((state) => state.classes.currClass);
 
     return (
-        <div className="announcement-header">
+        <div
+            className="announcement-header"
+            onClick={() => setWriteAssignment(true)}
+        >
             <img src={user.image} alt="user-img" className="user-img" />
             <div className="announce-content">
-                <h6 className="announce-heading" style={{ color: currClass.classTheme }}>
+                <h6
+                    className="announce-heading"
+                    style={{ color: currClass.classTheme }}
+                >
                     Announce something to your circle...
                 </h6>
                 <p className="announce-description">
-                    Share updates, announcements, or important information with your circle.
+                    Share updates, announcements, or important information with your
+                    circle.
                 </p>
             </div>
         </div>
     );
 };
 
-const AnnouncementWriter = ({ writeAssignment, announcement, handleAnnouncementChange, toggleWriteAssignment }) => {
+const AnnouncementWriter = ({
+    writeAssignment,
+    announcement,
+    handleAnnouncementChange,
+    toggleWriteAssignment,
+    handlePost,
+}) => {
     return (
         <div className="announcement-writer">
-            {writeAssignment ? (
-                <div className="announcement-editor">
-                    <TextField
-                        multiline
-                        autoFocus
-                        variant="standard"
-                        fullWidth
-                        value={announcement}
-                        onChange={handleAnnouncementChange}
-                        className="announcement-textfield no-border"
-                        InputProps={{
-                            disableUnderline: true
-                        }}
-                    />
-                    <Button variant="contained" color="primary" onClick={toggleWriteAssignment}>
-                        Post Announcement
-                    </Button>
+            <div className="announcement-editor">
+                <TextField
+                    multiline
+                    autoFocus
+                    variant="standard"
+                    value={announcement}
+                    onChange={handleAnnouncementChange}
+                    className="announcement-textfield no-border"
+                    InputProps={{
+                        style: {
+                            color: "#28231d",
+                            fontSize: '15px',
+                            paddingInline : '10px'
+                        },
+                        disableUnderline: true,
+                    }}
+                />
+                <div className="editor-controls">
+                    <div className="left-side-controllers">
+                        <IconButton color="primary" size="small">
+                            <FormatBold />
+                        </IconButton>
+                        <IconButton color="primary" size="small">
+                            <FormatItalic />
+                        </IconButton>
+                        <IconButton color="primary" size="small">
+                            <FormatUnderlined />
+                        </IconButton>
+                        <IconButton color="primary" size="small">
+                            <CloudUpload />
+                        </IconButton>
+                    </div>
+                    <div className="right-side-controllers">
+                        <IconButton
+                            color="secondary"
+                            size="small"
+                            onClick={toggleWriteAssignment}
+                        >
+                            Cancel
+                        </IconButton>
+                        <Button
+                            variant="contained"
+                            color="primary"
+                            onClick={handlePost}
+                            endIcon={
+                                <IoIosSend />
+                            }
+                        >
+                            Post
+                        </Button>
+                    </div>
                 </div>
-            ) : (
-                <Button variant="outlined" color="primary" onClick={toggleWriteAssignment}>
-                    Write Announcement
-                </Button>
-            )}
+            </div>
         </div>
     );
 };
@@ -62,18 +113,28 @@ export default function AnnouncementContainer() {
 
     const toggleWriteAssignment = () => {
         setWriteAssignment(!writeAssignment);
+        setAnnouncement("");
+    };
+
+    const handlePost = () => {
+        console.log("Post Announcement:", announcement);
+        setAnnouncement("");
+        setWriteAssignment(false);
     };
 
     return (
         <div className="main-announcement-container">
             <div className="announce-something-container">
-                <UserAnnouncementHeader />
-                <AnnouncementWriter
-                    writeAssignment={writeAssignment}
-                    announcement={announcement}
-                    handleAnnouncementChange={handleAnnouncementChange}
-                    toggleWriteAssignment={toggleWriteAssignment}
-                />
+                <UserAnnouncementHeader setWriteAssignment={setWriteAssignment} />
+                {writeAssignment && (
+                    <AnnouncementWriter
+                        writeAssignment={writeAssignment}
+                        announcement={announcement}
+                        handleAnnouncementChange={handleAnnouncementChange}
+                        toggleWriteAssignment={toggleWriteAssignment}
+                        handlePost={handlePost}
+                    />
+                )}
             </div>
         </div>
     );
