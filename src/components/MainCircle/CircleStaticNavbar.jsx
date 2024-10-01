2e63fa0aee9b6787927b45373f7380762e51fae8
yashpandav <pandavyash076@gmail.com>
2024-10-01 23:00:57 +0530

    ✔

--- src/components/MainCircle/CircleStaticNavbar.jsx
+++ src/components/MainCircle/CircleStaticNavbar.jsx
@@ -1,11 +1,20 @@
-import React, { useState } from "react";
+import React, { useState , useEffect } from "react";
 import { useSelector } from "react-redux";
 import SettingsOutlinedIcon from '@mui/icons-material/SettingsOutlined';
 import "./circleStaticNav.css";
 
 export default function CircleStaticNavbar() {
     const currClass = useSelector((state) => state.classes.currClass);
+    const currUser = useSelector((state) => state.auth.user);
+
     const [activeTab, setActiveTab] = useState("Stream");
+    const [isAdmin , setAdmin] = useState(false);
+
+    useEffect(() => {
+        if(currUser && currClass && currUser._id === currClass.admin._id){
+            setAdmin(true);
+        }
+    }, [currUser, currClass]);
 
     const navItems = ["Stream", "Classwork", "People"];
 
@@ -23,7 +32,7 @@ export default function CircleStaticNavbar() {
                         className="navbar-list"
                         onClick={() => setActiveTab(item)}
                         style={{
-                            backgroundColor: activeTab === item ? 'rgba(0, 0, 0, 0.04)' : 'transparent',
+                            backgroundColor: activeTab === item ? 'rgba(0, 0, 0, 0.03)' : 'transparent',
                         }}
                     >
                         <h3 style={{ color: activeTab === item ? currClass.classTheme : '#1a1a1a' }}>
@@ -32,9 +41,13 @@ export default function CircleStaticNavbar() {
                     </div>
                 ))}
             </div>
-            <div className="navbar-setting-class">
-                <SettingsOutlinedIcon className="settings-icon" />
-            </div>
+            {
+                isAdmin && (
+                    <div className="navbar-setting-class">
+                        <SettingsOutlinedIcon className="settings-icon" />
+                    </div>
+                )
+            }
         </div>
     );
 }
