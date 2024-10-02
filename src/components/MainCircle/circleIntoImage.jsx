adce4ad27076453ec515e301a090613a01f6d3b2
yashpandav <pandavyash076@gmail.com>
2024-10-02 12:59:49 +0530

    Added Customize Btn

--- src/components/MainCircle/circleIntoImage.jsx
+++ src/components/MainCircle/circleIntoImage.jsx
@@ -1,22 +1,27 @@
 import React, { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import InfoTwoToneIcon from "@mui/icons-material/InfoTwoTone";
+import CreateOutlinedIcon from '@mui/icons-material/CreateOutlined';
 import "./circleIntroImage.css";
 
 export default function CircleIntroImage() {
     const currClass = useSelector((state) => state.classes.currClass);
-    const [adminFirstName , setAdminFirstName] = useState(currClass.admin.firstName);
-    const [adminLastName , setAdminLastName] = useState(currClass.admin.lastName);
-
-    useEffect(
-        () => {
-            setAdminFirstName(currClass.admin.firstName);
-            setAdminLastName(currClass.admin.lastName);
-        },
-        []
-    );
-
+    const [adminFirstName, setAdminFirstName] = useState(currClass.admin.firstName);
+    const [adminLastName, setAdminLastName] = useState(currClass.admin.lastName);
+    const currUser = useSelector((state) => state.auth.user);
     const [toggleInfoContainer, setToggleInfoContainer] = useState(false);
+    const [isAdmin , setIsAdmin] = useState(false);
+
+    useEffect(() => {
+        setAdminFirstName(currClass.admin.firstName);
+        setAdminLastName(currClass.admin.lastName);
+    }, []);
+
+    useEffect(() =>{
+        if(currClass && currUser && currClass.admin._id === currUser._id){
+            setIsAdmin(true);
+        }
+    } , [currClass , currUser]);
 
     const toggleInfo = () => {
         setToggleInfoContainer(!toggleInfoContainer);
@@ -25,18 +30,23 @@ export default function CircleIntroImage() {
     return (
         <>
             <div
-                className={
-                    `circle-intro-image  ${toggleInfoContainer ? 'hide-border' : ''}`
-                }
+                className={`circle-intro-image ${toggleInfoContainer ? 'hide-border' : ''}`}
                 style={{ backgroundImage: `url(${currClass.thumbnail})` }}
             >
                 {
-                    toggleInfoContainer && (
-                        <span className="temp-circle-info-icon">
-                        </span>
+                    isAdmin && (
+                        <button className="customize-circle-btn">
+                            <CreateOutlinedIcon />
+                            <span>Customize</span>
+                        </button>
                     )
                 }
-                <div className={`circle-content`}>
+                
+                {toggleInfoContainer && (
+                    <span className="temp-circle-info-icon"></span>
+                )}
+                
+                <div className="circle-content">
                     <h3 className="curr-circle-name">{currClass.name}</h3>
                     <InfoTwoToneIcon
                         className={`info-icon ${toggleInfoContainer ? 'toggleInfoIcon' : ''}`}
@@ -44,22 +54,21 @@ export default function CircleIntroImage() {
                     />
                 </div>
             </div>
-            {
-                toggleInfoContainer && (
-                    <div className={`circle-info-container ${toggleInfoContainer ? 'show' : ''}`}>
-                        <div className="info-group">
-                            <h3 className="info-header">Created By: <span>{adminFirstName} {adminLastName}</span></h3>
-                            <h3 className="info-header">Description: <span>{currClass.description}</span></h3>
-                            <h3 className="info-header">Subject: <span>{currClass.subject}</span></h3>
-                        </div>
-                        <div className="info-group">
-                            <h3 className="info-header">Create Date: <span>{currClass.createDate}</span></h3>
-                            <h3 className="info-header">Total Students: <span>{currClass.student.length}</span></h3>
-                            <h3 className="info-header">Total Teachers: <span>{currClass.teacher.length}</span></h3>
-                        </div>
+            
+            {toggleInfoContainer && (
+                <div className={`circle-info-container ${toggleInfoContainer ? 'show' : ''}`}>
+                    <div className="info-group">
+                        <h3 className="info-header">Created By: <span>{adminFirstName} {adminLastName}</span></h3>
+                        <h3 className="info-header">Description: <span>{currClass.description}</span></h3>
+                        <h3 className="info-header">Subject: <span>{currClass.subject}</span></h3>
                     </div>
-                )
-            }
+                    <div className="info-group">
+                        <h3 className="info-header">Create Date: <span>{currClass.createDate}</span></h3>
+                        <h3 className="info-header">Total Students: <span>{currClass.student.length}</span></h3>
+                        <h3 className="info-header">Total Teachers: <span>{currClass.teacher.length}</span></h3>
+                    </div>
+                </div>
+            )}
         </>
     );
 }
